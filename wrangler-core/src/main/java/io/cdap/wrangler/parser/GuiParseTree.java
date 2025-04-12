ce7dd7a1fbde4e38daaa19236eb2fc0f59b95d80
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2025-04-12 09:13:53 +0530

    fixed some checkstyle issues

--- wrangler-core/src/main/java/io/cdap/wrangler/parser/GuiParseTree.java
+++ wrangler-core/src/main/java/io/cdap/wrangler/parser/GuiParseTree.java
@@ -3,26 +3,33 @@ package io.cdap.wrangler.parser;
 /*
  *  Copyright © 2017-2019 Cask Data, Inc.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
- *  use this file except in compliance with the License. You may obtain a copy of
- *  the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations under
- *  the License.
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
 import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
 
+/**
+ * Demonstrates how to use ANTLR's Trees GUI to visualize a parse tree
+ * for Wrangler's DSL using the Directives grammar.
+ */
 public class GuiParseTree {
     public static void main(String[] args) throws Exception {
-        String input = "aggregate-stats :col1 :col2 10KB 5sec;";
+        String input = "aggregate-stats :col1 :col2 90kb 5sec;";
 
         // Step 1: Create CharStream from input
         CharStream cs = CharStreams.fromString(input);
@@ -44,4 +51,3 @@ public class GuiParseTree {
         Trees.inspect(tree, parser);  // Launches GUI!
     }
 }
-
